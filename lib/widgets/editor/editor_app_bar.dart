959d0643edd102a188f25ed000c7a79535f9d622
qinscode <fudong.qin@outlook.com>
2024-11-09 00:53:05 +0800

    refactor: remove unused imports and delete empty adaptive icon file
    - Remove unused import of font_awesome_flutter in editor_app_bar.dart
    - Delete empty ic_launcher.xml file for adaptive icon

--- lib/widgets/editor/editor_app_bar.dart
+++ lib/widgets/editor/editor_app_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class EditorAppBar extends StatelessWidget implements PreferredSizeWidget {
   final bool isEdited;
